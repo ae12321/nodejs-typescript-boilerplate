80274233780e258b33c7dbf4bf87dbd99983c327
a.endo.w <80916217+ae12321@users.noreply.github.com>
2023-03-21 08:03:52 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import http from "http";
 import { sum } from "./lib/sum";
 
+function hello(name: string) {
+  return `Hello, ${name}`;
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  const result = sum(1, 2);
-  res.end(`hello, word, ${result}`);
+  const result = sum(1, 5);
+  res.end(`hello, word, ${result}, ${hello("ss")}`);
 });
 
 server.listen(8080);
